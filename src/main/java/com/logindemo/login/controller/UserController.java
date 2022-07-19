40ae8fbf19e0a2ed1286e71f69a950ee7e2c9f61
Sridharan <sridharan@divum.in>
2022-07-19 17:16:50 +0530

    updated changes

--- src/main/java/com/logindemo/login/controller/UserController.java
+++ src/main/java/com/logindemo/login/controller/UserController.java
@@ -8,9 +8,6 @@ import com.logindemo.login.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
